e1b602e748f5e0761f19f1c12077a875b707b2ff
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-12 17:13:36 +0100

    Create array of car brands.

---
+++ script.js
@@ -0,0 +1,27 @@
+const database = [
+  {
+    carName: 'Ford',
+    carPrice: 17000,
+    carCode: 'FD',
+  },
+  {
+    carName: 'Toyota',
+    carPrice: 17000,
+    carCode: 'TY',
+  },
+  {
+    carName: 'Vauxhall',
+    carPrice: 17000,
+    carCode: 'VX',
+  },
+  {
+    carName: 'Mercedes',
+    carPrice: 17000,
+    carCode: 'MC',
+  },
+  {
+    carName: 'Volkswagen',
+    carPrice: 17000,
+    carCode: 'VW',
+  },
+];
