946e92bd49fa0e25b5b0d15d3ae33ee46c9dcb51
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-12 17:31:35 +0100

    Create function to get results from the searchbar.

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ const database = [
 function selectElement(selector) {
   return document.querySelector(selector);
 }
+
+function getResults() {
+  const search = selectElement('.searchbar').value;
+
+  for (let i = 0; i < database.length; i++) {
+    if (
+      database[i].carName
+        .toLocaleLowerCase()
+        .includes(search.toLocaleLowerCase()) ||
+      database[i].carCode
+        .toLocaleLowerCase()
+        .includes(search.toLocaleLowerCase())
+    ) {
+      selectElement('.search-results').innerHTML += `
+
+        <div class="search-results-item">
+          <span class="search-item>${database[i].carName}</span>"
+          <span class="search-item>${database[i].carPrice}</span>"
+          <span class="search-item>${database[i].carCode}</span>"
+        </div>
+      `;
+    }
+  }
+}
