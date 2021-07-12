6578739e9a5d5df892f83ce39c656d9bfc8d6e68
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-12 17:48:28 +0100

    Add if statement to remove old data.

--- script.js
+++ script.js
@@ -40,23 +40,26 @@ function getResults() {
   const search = selectElement('.searchbar').value;
 
   clearResults();
-  for (let i = 0; i < database.length; i++) {
-    if (
-      database[i].carName
-        .toLocaleLowerCase()
-        .includes(search.toLocaleLowerCase()) ||
-      database[i].carCode
-        .toLocaleLowerCase()
-        .includes(search.toLocaleLowerCase())
-    ) {
-      selectElement('.search-results').innerHTML += `
 
-        <div class="search-results-item">
-          <span class="search-item">${database[i].carName}</span>
-          <span class="search-item">${database[i].carPrice}</span>
-          <span class="search-item">${database[i].carCode}</span>
-        </div>
-      `;
+  if (search.length > 0) {
+    for (let i = 0; i < database.length; i++) {
+      if (
+        database[i].carName
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase()) ||
+        database[i].carCode
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase())
+      ) {
+        selectElement('.search-results').innerHTML += `
+  
+          <div class="search-results-item">
+            <span class="search-item">${database[i].carName}</span>
+            <span class="search-item">${database[i].carPrice}</span>
+            <span class="search-item">${database[i].carCode}</span>
+          </div>
+        `;
+      }
     }
   }
 }
